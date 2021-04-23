a04556ce83b6c4e9d13b3157f0877e7ba7b8ad38
Jeremias <jdossantospinheiro20@gmail.com>
2021-04-23 11:48:02 -0300

    updating tests

--- src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -8,9 +8,9 @@ import java.util.Collections;
 
 public class PersonUtils {
 
-    private static final String FIRST_NAME = "Rodrigo";
-    private static final String LAST_NAME = "Peleias";
-    private static final String CPF_NUMBER = "369.333.878-79";
+    private static final String FIRST_NAME = "Jeremias";
+    private static final String LAST_NAME = "Pinheiro";
+    private static final String CPF_NUMBER = "359.323.354-00";
     private static final long PERSON_ID = 1L;
     public static final LocalDate BIRTH_DATE = LocalDate.of(2010, 10, 1);
 
@@ -19,7 +19,7 @@ public class PersonUtils {
                 .firstName(FIRST_NAME)
                 .lastName(LAST_NAME)
                 .cpf(CPF_NUMBER)
-                .birthDate("04-04-2010")
+                .birthDate("04-04-2015")
                 .phones(Collections.singletonList(PhoneUtils.createFakeDTO()))
                 .build();
     }
